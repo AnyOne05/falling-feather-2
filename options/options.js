e16d6c886abb1526c8240a4a264a643e3341be5b
django <django@e59df45c5270>
2024-06-20 20:31:13 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "falling-feather-2",
+  url: "https://falling-feather-2.botics.co",
+  api: "https://falling-feather-2.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
